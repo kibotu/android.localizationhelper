d6fc95aebde92406a03139bd21cbf2015d72a77d
Jan Rabe <jan.rabe@exozet.com>
2015-05-19 13:14:51 +0200

    init

---
+++ src/com/exozet/Main.java
@@ -0,0 +1,46 @@
+package com.exozet;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class Main {
+
+    public static void main(final String... args) throws Exception {
+
+        final String pathname = "assets/strings.xml";
+        final File source = new File(pathname);
+        if (!source.exists()) {
+            System.out.println("'" + pathname + " ' File doesn't exist.");
+            return;
+        }
+
+        final SessionIdentifierGenerator generator = new SessionIdentifierGenerator();
+
+        try {
+
+            // load and parse
+            final SAXBuilder builder = new SAXBuilder();
+            final Document doc = builder.build(source);
+            final XMLOutputter fmt = new XMLOutputter();
+            final Element resource = doc.getRootElement();
+
+            // update fields with garbage
+            for (final Element element : resource.getChildren("string"))
+                element.setText(generator.nextSessionId().substring(10).toUpperCase());
+
+            // store
+            final FileWriter writer = new FileWriter(pathname);
+            fmt.output(doc, writer);
+            writer.flush();
+            writer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/exozet/SessionIdentifierGenerator.java
@@ -0,0 +1,15 @@
+package com.exozet;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * Created by Jan Rabe on 19/05/15.
+ */
+public class SessionIdentifierGenerator {
+    private SecureRandom random = new SecureRandom();
+
+    public String nextSessionId() {
+        return new BigInteger(130, random).toString(32);
+    }
+}
